b0311372e969623abcfd53742f49928b751d30d9
lucian.stoicescu <lucian.stoicescu@Lucian-Stoicescus-MacBook-Pro.local>
2020-01-28 17:06:19 -0500

    ENG-3560 Stable test for normal chrome
    Think happy thoughts for the rest

--- grommet_regression/test/specs/basic.js
+++ grommet_regression/test/specs/basic.js
@@ -1,13 +1,28 @@
 const assert = require('assert')
 describe('The Grommet main page', ()=>{
+
   it('Should Have a Title',()=> {
    browser.url('https://dev.thegrommet.com')
    const title = browser.getTitle()
-   assert(true,title.includes('The Grommet'))
+   assert(title.includes('The Grommit'),"Actual title displayed was: "+title)
  });
   it('Should go to a product page', ()=> {
-    const disc = $('//div[@class="tile-container-3 tile-container-2-mobile"]/a[1]')
+    const disc = $('//div[@class="tile-container-3 tile-container-2-mobile"]/a[2]')
     disc.click()
-    browser.pause(15000)
   });
+  it('Should select a product', ()=> {
+    const prod = $('//div[@class="tile-container-3 tile-spacing-4 tile-container-2-tablet tile-spacing-2-mobile "]/a[2]')
+    prod.click()
+  });
+  it('Should increment the product amount to 3 and add to cart', ()=> {
+    const amount = $("//select/option[text()='3']")
+    amount.click()
+    const btn = $('//*[@id="cart_button_state"]/button')
+    btn.click()
+    browser.pause(10000)
+  });
+  it('Should display 4 items in the mini-cart', ()=> {
+    assert($('//div[@class="flex-row flex-justify-center"]').getText().includes("4 items in your cart"),"Was actually: "+$('//div[@class="flex-row flex-justify-center"]').getText())
+  });
+
 })
